fb274e38b6551cf211b3e1efa7c7bf1f667f4ee2
Ïzabela Gomes <igomes@pivotal.io>
2017-01-26 15:30:36 -0500

    WIP: Added NetworkTask to call backend

---
+++ app/src/main/java/com/example/pivotal/boomerang_pivotal/model/JsonObject.java
@@ -0,0 +1,58 @@
+package com.example.pivotal.boomerang_pivotal.model;
+
+public class JsonObject {
+    String title;
+    String address;
+    String hours;
+    String description;
+    Float latitude;
+    Float longitude;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+}
