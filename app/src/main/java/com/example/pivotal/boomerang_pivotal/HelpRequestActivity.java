a045c24721276514d94d432e9564f22813aad636
Dev Floater 118 <pivotal@Dev-Floater-118.local>
2017-01-26 21:26:24 -0500

    Added I need help screens

--- app/src/main/java/com/example/pivotal/boomerang_pivotal/HelpRequestActivity.java
+++ app/src/main/java/com/example/pivotal/boomerang_pivotal/HelpRequestActivity.java
@@ -1,11 +1,15 @@
 package com.example.pivotal.boomerang_pivotal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.pivotal.boomerang_pivotal.service.NetworkPostTask;
 
 public class HelpRequestActivity extends AppCompatActivity {
 
@@ -15,15 +19,27 @@ public class HelpRequestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help_request);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    public void onAskForHelpClicked(View view) {
+        EditText noteView = (EditText) findViewById(R.id.note_edit);
+        String note = noteView.getText().toString();
+
+        EditText titleView = (EditText) findViewById(R.id.title_edit);
+        String title = titleView.getText().toString();
+
+        EditText timeView = (EditText) findViewById(R.id.time_edit);
+        String hours = timeView.getText().toString();
+
+        EditText personView = (EditText) findViewById(R.id.person_edit);
+        String user = personView.getText().toString();
+
+        EditText locationView = (EditText) findViewById(R.id.location_edit);
+        String location = locationView.getText().toString();
+
+        new NetworkPostTask().execute("https://boomerang.cfapps.io/opportunity");
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/pivotal/boomerang_pivotal/MainActivity.java
+++ app/src/main/java/com/example/pivotal/boomerang_pivotal/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.pivotal.boomerang_pivotal;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
     }
 
     @Override
